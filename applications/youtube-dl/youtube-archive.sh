a2eae26969accb8b7e0ad105048b295fe5af0c11
gncabrera <gcabrera@kirios.com.ar>
2022-02-12 17:31:34 -0300

    Refactor /mnt/media > /mnt/storage

--- applications/youtube-dl/youtube-archive.sh
+++ applications/youtube-dl/youtube-archive.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#/usr/local/bin/youtube-dl --config-location /mnt/Media/YouTube/youtube-dl.conf -o "/mnt/Media/YouTube/Playlists/%(playlist)s/%(playlist)s - S01E%(playlist_index)s - %(title)s [%(id)s].%(ext)s" --batch-file=/mnt/Media/YouTube/playlist_list.txt
+#/usr/local/bin/youtube-dl --config-location /mnt/storage/YouTube/youtube-dl.conf -o "/mnt/storage/YouTube/Playlists/%(playlist)s/%(playlist)s - S01E%(playlist_index)s - %(title)s [%(id)s].%(ext)s" --batch-file=/mnt/storage/YouTube/playlist_list.txt
 
 /usr/local/bin/youtube-dl --config-location /opt/kirios/applications/youtube-dl/youtube-dl.conf -o "~/YouTube/%(uploader)s/%(playlist)s/%(playlist)s - S01E%(playlist_index)s - %(title)s [%(id)s].%(ext)s" --batch-file=/opt/kirios/applications/youtube-dl/channel_list.txt
